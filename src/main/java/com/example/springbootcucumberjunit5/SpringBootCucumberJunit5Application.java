1166a0a3eb12e7df1898fbae0acfeca8c8c76c30
Jean-Luc Mounsamy <jl.mounsamy@gmail.com>
2022-04-08 21:34:23 -0400

    Initial commit

---
+++ src/main/java/com/example/springbootcucumberjunit5/SpringBootCucumberJunit5Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootcucumberjunit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCucumberJunit5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCucumberJunit5Application.class, args);
+    }
+
+}
